48e66ed6279557d67d3a917573ee7c8c45c2e263
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2024-02-06 10:27:16 +0000

    Create a post method for message router

--- routes/message.js
+++ routes/message.js
@@ -2,7 +2,12 @@ const express = require("express")
 const router = express.Router()
 
 router.get("/", (req, res) => {
-    res.send("NOT IMPLEMENTED")
+    res.render("form")
+})
+
+router.post("/", (req, res) => {
+    messages.push({text: messageText, user: messageUser, added: new Date()});
+    res.redirect("/")
 })
 
 module.exports = router
