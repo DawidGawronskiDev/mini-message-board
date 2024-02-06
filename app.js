45baf220e130b748b05b65d256dbee5e5e6fbac7
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2024-02-06 09:46:29 +0000

    Fix view engine

--- app.js
+++ app.js
@@ -2,12 +2,18 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const exphbs = require("express-handlebars");
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-const messageRouter = require("./routes/message")
+const messageRouter = require("./routes/message");
 
 const app = express();
+
+// Sets handlebars configurations (we will go through them later on)
+app.engine('handlebars', exphbs.engine({ defaultLayout: "main" }));
+app.set('view engine', 'handlebars');
+app.set('views', './views');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -17,6 +23,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use("/new", messageRouter)
+app.use("/new", messageRouter);
 
 module.exports = app;
