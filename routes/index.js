216592644c50be0597756f70c614be286a691044
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2024-02-06 10:34:46 +0000

    Move Messages

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-const messages = [
-  {
-    text: "Hi there!",
-    user: "Amando",
-    added: new Date()
-  },
-  {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date()
-  }
-];
+const messages = require("../Messages")
 
 /* GET home page. */
 router.get('/', function(req, res) {
--- routes/message.js
+++ routes/message.js
@@ -1,12 +1,14 @@
 const express = require("express")
 const router = express.Router()
 
+const messages = require("../Messages")
+
 router.get("/", (req, res) => {
     res.render("form")
 })
 
 router.post("/", (req, res) => {
-    messages.push({text: messageText, user: messageUser, added: new Date()});
+    messages.push({text: req.body.messageText, user: req.body.messageUser, added: new Date()});
     res.redirect("/")
 })
 
