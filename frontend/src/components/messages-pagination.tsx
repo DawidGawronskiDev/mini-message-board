b67c54e5e7ffaa45c5cc078a519a51cee99fdaeb
DawidGawronskiDev <dawidgawronskicontact@gmail.com>
2024-08-04 16:43:36 +0100

    Update pagination

--- frontend/src/components/messages-pagination.tsx
+++ frontend/src/components/messages-pagination.tsx
@@ -1,8 +1,11 @@
 import {
   Pagination,
   PaginationContent,
+  PaginationEllipsis,
   PaginationItem,
   PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
 } from "./ui/pagination";
 
 type MessagesPaginationProps = {
@@ -16,19 +19,73 @@ const MessagesPagination = ({
   handleCurrentPage,
   size,
 }: MessagesPaginationProps) => {
+  console.log(currentPage, Math.round(size));
   return (
     <Pagination>
       <PaginationContent>
-        {new Array(Math.ceil(size)).fill("_").map((_, index) => (
-          <PaginationItem key={index}>
-            <PaginationLink
-              onClick={() => handleCurrentPage(index + 1)}
-              isActive={currentPage === index + 1}
-            >
-              {index + 1}
-            </PaginationLink>
+        <PaginationItem>
+          <PaginationPrevious
+            onClick={() =>
+              handleCurrentPage(currentPage <= 1 ? 1 : currentPage - 1)
+            }
+          />
+        </PaginationItem>
+        {new Array(Math.ceil(size)).fill("_").map((_, index) => {
+          if (currentPage === 1 && index < 3) {
+            return (
+              <PaginationItem key={index}>
+                <PaginationLink
+                  onClick={() => handleCurrentPage(index + 1)}
+                  isActive={currentPage === index + 1}
+                >
+                  {index + 1}
+                </PaginationLink>
+              </PaginationItem>
+            );
+          }
+          if (currentPage === Math.round(size + 1) && index > size - 3) {
+            return (
+              <PaginationItem key={index}>
+                <PaginationLink
+                  onClick={() => handleCurrentPage(index + 1)}
+                  isActive={currentPage === index + 1}
+                >
+                  {index + 1}
+                </PaginationLink>
+              </PaginationItem>
+            );
+          }
+          if (
+            currentPage > 1 &&
+            index > currentPage - 3 &&
+            index < currentPage + 1
+          ) {
+            return (
+              <PaginationItem key={index}>
+                <PaginationLink
+                  onClick={() => handleCurrentPage(index + 1)}
+                  isActive={currentPage === index + 1}
+                >
+                  {index + 1}
+                </PaginationLink>
+              </PaginationItem>
+            );
+          }
+        })}
+        {currentPage > 1 && currentPage < size - 1 && (
+          <PaginationItem>
+            <PaginationEllipsis />
           </PaginationItem>
-        ))}
+        )}
+        <PaginationItem>
+          <PaginationNext
+            onClick={() =>
+              handleCurrentPage(
+                currentPage >= size ? Math.floor(size + 1) : currentPage + 1
+              )
+            }
+          />
+        </PaginationItem>
       </PaginationContent>
     </Pagination>
   );
