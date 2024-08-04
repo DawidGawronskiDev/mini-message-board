1af2609d0766ec85a0e42c21fa49dea7ae068347
DawidGawronskiDev <dawidgawronskicontact@gmail.com>
2024-08-04 17:39:17 +0100

    Update

--- backend/src/routes/messagesRoutes.ts
+++ backend/src/routes/messagesRoutes.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import readMessages from "../util/readMessages";
 import writeMessage from "../util/writeMessage";
 import createMessage from "../util/createMessage";
+import z from "zod";
 const router = express.Router();
 
 router.get("/", (req: Request, res: Response) => {
@@ -9,11 +10,24 @@ router.get("/", (req: Request, res: Response) => {
   res.json(messages);
 });
 
+const messageSchema = z.object({
+  user: z
+    .string()
+    .min(3, { message: "Username must contain at least 3 character(s)" })
+    .max(32, { message: "Username can't be longer than 32 character(s)" }),
+  text: z
+    .string()
+    .min(3, { message: "Message must contain at least 3 character(s)" })
+    .max(255, { message: "Username can't be longer than 255 character(s)" }),
+});
+
 router.post("/", (req: Request, res: Response) => {
   const { user, text } = req.body;
 
-  if (!user || !text) {
-    return res.status(404).json({ message: "User and text are required." });
+  const result = messageSchema.safeParse(req.body);
+
+  if (!result.success) {
+    return res.status(400).json({ errors: result.error.errors });
   }
 
   writeMessage([createMessage(user, text), ...readMessages()]);
